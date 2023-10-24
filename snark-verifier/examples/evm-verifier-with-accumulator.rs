9653c7b838f6bb4940d433d1c7afe28356ccbbfd
caojiafeng <funfriendcjf@gmail.com>
2023-10-24 17:37:29 +0800

    add test

--- snark-verifier/examples/evm-verifier-with-accumulator.rs
+++ snark-verifier/examples/evm-verifier-with-accumulator.rs
@@ -1,18 +1,13 @@
 use halo2_curves::bn256::{Bn256, Fq, Fr, G1Affine};
-use halo2_proofs::{
-    dev::MockProver,
-    plonk::{create_proof, keygen_pk, keygen_vk, verify_proof, Circuit, ProvingKey, VerifyingKey},
-    poly::{
-        commitment::{Params, ParamsProver},
-        kzg::{
-            commitment::{KZGCommitmentScheme, ParamsKZG},
-            multiopen::{ProverGWC, VerifierGWC},
-            strategy::AccumulatorStrategy,
-        },
-        VerificationStrategy,
+use halo2_proofs::{dev::MockProver, plonk::{create_proof, keygen_pk, keygen_vk, verify_proof, Circuit, ProvingKey, VerifyingKey}, poly::{
+    commitment::{Params, ParamsProver},
+    kzg::{
+        commitment::{KZGCommitmentScheme, ParamsKZG},
+        multiopen::{ProverGWC, VerifierGWC},
+        strategy::AccumulatorStrategy,
     },
-    transcript::{EncodedChallenge, TranscriptReadBuffer, TranscriptWriterBuffer},
-};
+    VerificationStrategy,
+}, SerdeFormat, transcript::{EncodedChallenge, TranscriptReadBuffer, TranscriptWriterBuffer}};
 use itertools::Itertools;
 use rand::rngs::OsRng;
 use snark_verifier::{
@@ -592,28 +587,320 @@ fn evm_verify(deployment_code: Vec<u8>, instances: Vec<Vec<Fr>>, proof: Vec<u8>)
 }
 
 fn main() {
-    let params = gen_srs(22);
+    let params = gen_srs(21);
     let params_app = {
         let mut params = params.clone();
         params.downsize(8);
         params
     };
 
-    let snarks = [(); 3].map(|_| gen_application_snark(&params_app));
+    let snarks = [(); 2].map(|_| gen_application_snark(&params_app));
     let agg_circuit = aggregation::AggregationCircuit::new(&params, snarks);
-    let pk = gen_pk(&params, &agg_circuit);
+    let pk1 = gen_pk(&params, &agg_circuit);
     let deployment_code = gen_aggregation_evm_verifier(
         &params,
-        pk.get_vk(),
+        pk1.get_vk(),
         aggregation::AggregationCircuit::num_instance(),
         aggregation::AggregationCircuit::accumulator_indices(),
     );
 
-    let proof = gen_proof::<_, _, EvmTranscript<G1Affine, _, _, _>, EvmTranscript<G1Affine, _, _, _>>(
-        &params,
-        &pk,
-        agg_circuit.clone(),
-        agg_circuit.instances(),
-    );
-    evm_verify(deployment_code, agg_circuit.instances(), proof);
+    let snarks = {
+        const W: usize = 4;
+        const H: usize = 32;
+        const K: u32 = 8;
+        let snark1= gen_application_snark(&params_app);
+        let circuit = shuffle::MyCircuit::<_, W, H>::rand(&mut OsRng);
+        let snark2 = {
+            let mut params = params.clone();
+            params.downsize(15);
+            let pk = gen_pk(&params, &circuit);
+            let protocol = compile(
+                &params,
+                pk.get_vk(),
+                Config::kzg(),
+            );
+
+            let proof = gen_proof::<
+                _,
+                _,
+                aggregation::PoseidonTranscript<NativeLoader, _>,
+                aggregation::PoseidonTranscript<NativeLoader, _>,
+            >(&params, &pk, circuit.clone(), vec![]);
+            aggregation::Snark::new(protocol, vec![], proof)
+        };
+        let snarks = [snark1,snark2];
+        snarks
+    };
+
+    let agg_circuit = aggregation::AggregationCircuit::new(&params, snarks);
+    let pk2 = gen_pk(&params, &agg_circuit);
+
+    {
+        let vk1 = pk1.get_vk().to_bytes(SerdeFormat::RawBytes);
+        let vk2 = pk2.get_vk().to_bytes(SerdeFormat::RawBytes);
+        assert_eq!(vk1, vk2);
+    }
+
+
+    // let proof = gen_proof::<_, _, EvmTranscript<G1Affine, _, _, _>, EvmTranscript<G1Affine, _, _, _>>(
+    //     &params,
+    //     &pk,
+    //     agg_circuit.clone(),
+    //     agg_circuit.instances(),
+    // );
+    // evm_verify(deployment_code, agg_circuit.instances(), proof);
+}
+
+
+pub(crate) mod shuffle {
+    use halo2_curves::ff::{FromUniformBytes,BatchInvert};
+    use halo2_proofs::{
+        arithmetic::{CurveAffine, Field},
+        circuit::{floor_planner::V1, Layouter, Value},
+        dev::{metadata, FailureLocation, MockProver, VerifyFailure},
+        halo2curves::pasta::EqAffine,
+        plonk::*,
+        poly::{
+            commitment::ParamsProver,
+            ipa::{
+                commitment::{IPACommitmentScheme, ParamsIPA},
+                multiopen::{ProverIPA, VerifierIPA},
+                strategy::AccumulatorStrategy,
+            },
+            VerificationStrategy,
+        },
+        transcript::{
+            Blake2bRead, Blake2bWrite, Challenge255, TranscriptReadBuffer, TranscriptWriterBuffer,
+        },
+    };
+    use rand::rngs::OsRng;
+    use rand::RngCore;
+    use std::iter;
+    use snark_verifier::util::arithmetic::batch_invert;
+
+    fn rand_2d_array<F: Field, R: RngCore, const W: usize, const H: usize>(rng: &mut R) -> [[F; H]; W] {
+        [(); W].map(|_| [(); H].map(|_| F::random(&mut *rng)))
+    }
+
+    fn shuffled<F: Field, R: RngCore, const W: usize, const H: usize>(
+        original: [[F; H]; W],
+        rng: &mut R,
+    ) -> [[F; H]; W] {
+        let mut shuffled = original;
+
+        for row in (1..H).rev() {
+            let rand_row = (rng.next_u32() as usize) % row;
+            for column in shuffled.iter_mut() {
+                column.swap(row, rand_row);
+            }
+        }
+
+        shuffled
+    }
+
+    #[derive(Clone)]
+    pub(crate) struct MyConfig<const W: usize> {
+        q_shuffle: Selector,
+        q_first: Selector,
+        q_last: Selector,
+        original: [Column<Advice>; W],
+        shuffled: [Column<Advice>; W],
+        theta: Challenge,
+        gamma: Challenge,
+        z: Column<Advice>,
+    }
+
+    impl<const W: usize> MyConfig<W> {
+        fn configure<F: Field>(meta: &mut ConstraintSystem<F>) -> Self {
+            let [q_shuffle, q_first, q_last] = [(); 3].map(|_| meta.selector());
+            // First phase
+            let original = [(); W].map(|_| meta.advice_column_in(FirstPhase));
+            let shuffled = [(); W].map(|_| meta.advice_column_in(FirstPhase));
+            let [theta, gamma] = [(); 2].map(|_| meta.challenge_usable_after(FirstPhase));
+            // Second phase
+            let z = meta.advice_column_in(SecondPhase);
+
+            meta.create_gate("z should start with 1", |_| {
+                let one = Expression::Constant(F::ONE);
+
+                vec![q_first.expr() * (one - z.cur())]
+            });
+
+            meta.create_gate("z should end with 1", |_| {
+                let one = Expression::Constant(F::ONE);
+
+                vec![q_last.expr() * (one - z.cur())]
+            });
+
+            meta.create_gate("z should have valid transition", |_| {
+                let q_shuffle = q_shuffle.expr();
+                let original = original.map(|advice| advice.cur());
+                let shuffled = shuffled.map(|advice| advice.cur());
+                let [theta, gamma] = [theta, gamma].map(|challenge| challenge.expr());
+
+                // Compress
+                let original = original
+                    .iter()
+                    .cloned()
+                    .reduce(|acc, a| acc * theta.clone() + a)
+                    .unwrap();
+                let shuffled = shuffled
+                    .iter()
+                    .cloned()
+                    .reduce(|acc, a| acc * theta.clone() + a)
+                    .unwrap();
+
+                vec![q_shuffle * (z.cur() * (original + gamma.clone()) - z.next() * (shuffled + gamma))]
+            });
+
+            Self {
+                q_shuffle,
+                q_first,
+                q_last,
+                original,
+                shuffled,
+                theta,
+                gamma,
+                z,
+            }
+        }
+    }
+
+    #[derive(Clone, Default)]
+    pub(crate) struct MyCircuit<F: Field, const W: usize, const H: usize> {
+        original: Value<[[F; H]; W]>,
+        shuffled: Value<[[F; H]; W]>,
+    }
+
+    impl<F: Field, const W: usize, const H: usize> MyCircuit<F, W, H> {
+        pub(crate) fn rand<R: RngCore>(rng: &mut R) -> Self {
+            let original = rand_2d_array::<F, _, W, H>(rng);
+            let shuffled = shuffled(original, rng);
+
+            Self {
+                original: Value::known(original),
+                shuffled: Value::known(shuffled),
+            }
+        }
+    }
+
+    impl<F: Field, const W: usize, const H: usize> Circuit<F> for MyCircuit<F, W, H> {
+        type Config = MyConfig<W>;
+        type FloorPlanner = V1;
+        #[cfg(feature = "circuit-params")]
+        type Params = ();
+
+        fn without_witnesses(&self) -> Self {
+            Self::default()
+        }
+
+        fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+            MyConfig::configure(meta)
+        }
+
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<F>,
+        ) -> Result<(), Error> {
+            let theta = layouter.get_challenge(config.theta);
+            let gamma = layouter.get_challenge(config.gamma);
+
+            layouter.assign_region(
+                || "Shuffle original into shuffled",
+                |mut region| {
+                    // Keygen
+                    config.q_first.enable(&mut region, 0)?;
+                    config.q_last.enable(&mut region, H)?;
+                    for offset in 0..H {
+                        config.q_shuffle.enable(&mut region, offset)?;
+                    }
+
+                    // First phase
+                    for (idx, (&column, values)) in config
+                        .original
+                        .iter()
+                        .zip(self.original.transpose_array().iter())
+                        .enumerate()
+                    {
+                        for (offset, &value) in values.transpose_array().iter().enumerate() {
+                            region.assign_advice(
+                                || format!("original[{}][{}]", idx, offset),
+                                column,
+                                offset,
+                                || value,
+                            )?;
+                        }
+                    }
+                    for (idx, (&column, values)) in config
+                        .shuffled
+                        .iter()
+                        .zip(self.shuffled.transpose_array().iter())
+                        .enumerate()
+                    {
+                        for (offset, &value) in values.transpose_array().iter().enumerate() {
+                            region.assign_advice(
+                                || format!("shuffled[{}][{}]", idx, offset),
+                                column,
+                                offset,
+                                || value,
+                            )?;
+                        }
+                    }
+
+                    // Second phase
+                    let z = self.original.zip(self.shuffled).zip(theta).zip(gamma).map(
+                        |(((original, shuffled), theta), gamma)| {
+                            let mut product = vec![F::ZERO; H];
+                            for (idx, product) in product.iter_mut().enumerate() {
+                                let mut compressed = F::ZERO;
+                                for value in shuffled.iter() {
+                                    compressed *= theta;
+                                    compressed += value[idx];
+                                }
+
+                                *product = compressed + gamma
+                            }
+
+                            product.iter_mut().batch_invert();
+
+                            for (idx, product) in product.iter_mut().enumerate() {
+                                let mut compressed = F::ZERO;
+                                for value in original.iter() {
+                                    compressed *= theta;
+                                    compressed += value[idx];
+                                }
+
+                                *product *= compressed + gamma
+                            }
+
+                            #[allow(clippy::let_and_return)]
+                                let z = iter::once(F::ONE)
+                                .chain(product)
+                                .scan(F::ONE, |state, cur| {
+                                    *state *= &cur;
+                                    Some(*state)
+                                })
+                                .collect::<Vec<_>>();
+
+                            #[cfg(feature = "sanity-checks")]
+                            assert_eq!(F::ONE, *z.last().unwrap());
+
+                            z
+                        },
+                    );
+                    for (offset, value) in z.transpose_vec(H + 1).into_iter().enumerate() {
+                        region.assign_advice(
+                            || format!("z[{}]", offset),
+                            config.z,
+                            offset,
+                            || value,
+                        )?;
+                    }
+
+                    Ok(())
+                },
+            )
+        }
+    }
 }
